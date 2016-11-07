2ef98ed1a17620eece57e9b11b6bb45b8a79701b
gaothing <744989745@qq.com>
2016-11-07 15:08:03 +0800

    changs music click

--- js/music.js
+++ js/music.js
@@ -1,799 +1,34 @@
 $(function() {
-	var database = [
-{
-			id: "1",
-			songs: "幸福的错觉",
-			name: "任重",
-			src: "music/幸福的错觉.mp3",
-			alltime: "4:36",
-			photo: "img/rz.jpg",
-			lyrics: [{
-				time: "0:00",
-				lyric: " "
-			}, {
-				time: "0:01",
-				lyric: "幸福的错觉"
-			}, {
-				time: "0:02",
-				lyric: " "
-			}, {
-				time: "0:03",
-				lyric: "演唱：任重"
-			}, {
-				time: "0:04",
-				lyric: "作曲：祁勃力"
-			}, {
-				time: "0:06",
-				lyric: "作词：樊凡 孙艺 祁勃力"
-			}, {
-				time: "0:08",
-				lyric: ""
-			}, {
-				time: "0:18",
-				lyric: "祝福明天"
-			}, {
-				time: "0:21",
-				lyric: "我不该就这样走远"
-			}, {
-				time: "0:25",
-				lyric: "假设了千万遍"
-			}, {
-				time: "0:27",
-				lyric: "又如何能改变"
-			}, {
-				time: "0:32",
-				lyric: "我们的这些天"
-			}, {
-				time: "0:35",
-				lyric: "我回想你的脸"
-			}, {
-				time: "0:37",
-				lyric: "怎么说再见"
-			}, {
-				time: "0:40",
-				lyric: "画面 冻结在一瞬间"
-			}, {
-				time: "0:46",
-				lyric: ""
-			}, {
-				time: "0:47",
-				lyric: "时间 留下了无解的思念"
-			}, {
-				time: "0:55",
-				lyric: "回忆敌不过"
-			}, {
-				time: "0:58",
-				lyric: "距离的遥远"
-			}, {
-				time: "1:01",
-				lyric: "那些往昔"
-			}, {
-				time: "1:04",
-				lyric: "如童话般美丽的记忆"
-			}, {
-				time: "1:10",
-				lyric: "如今 却已散落一地"
-			}, {
-				time: "1:16",
-				lyric: ""
-			}, {
-				time: "1:16",
-				lyric: "怎样才可以"
-			}, {
-				time: "1:19",
-				lyric: "说服了我自己"
-			}, {
-				time: "1:24",
-				lyric: "我们拥有的曾经"
-			}, {
-				time: "1:28",
-				lyric: "并不算爱情"
-			}, {
-				time: "1:31",
-				lyric: "为什么忽然间"
-			}, {
-				time: "1:35",
-				lyric: "我差一点相信"
-			}, {
-				time: "1:39",
-				lyric: "完美的结局"
-			}, {
-				time: "1:41",
-				lyric: "属于我和你"
-			}, {
-				time: "1:45",
-				lyric: ""
-			}, {
-				time: "1:45",
-				lyric: "怎样才可以"
-			}, {
-				time: "1:48",
-				lyric: ""
-			}, {
-				time: "1:54",
-				lyric: "彻底把过去"
-			}, {
-				time: "1:56",
-				lyric: "在心底抹去"
-			}, {
-				time: "2:00",
-				lyric: "如果说那幸福"
-			}, {
-				time: "2:04",
-				lyric: "不过都是错觉"
-			}, {
-				time: "2:09",
-				lyric: "为何有微笑"
-			}, {
-				time: "2:11",
-				lyric: "留在照片里"
-			}, {
-				time: "2:15",
-				lyric: " "
-			}, {
-				time: "2:16",
-				lyric: " "
-			}, {
-				time: "2:41",
-				lyric: "时间 留下了无解的思念"
-			}, {
-				time: "2:49",
-				lyric: "回忆敌不过"
-			}, {
-				time: "2:51",
-				lyric: "距离的遥远"
-			}, {
-				time: "2:56",
-				lyric: "那些往昔"
-			}, {
-				time: "2:58",
-				lyric: "如童话般美丽的记忆"
-			}, {
-				time: "3:04",
-				lyric: "如今 却已散落一地"
-			}, {
-				time: "3:10",
-				lyric: " "
-			}, {
-				time: "3:10",
-				lyric: "怎样才可以"
-			}, {
-				time: "3:13",
-				lyric: "说服了我自己"
-			}, {
-				time: "3:19",
-				lyric: "我们拥有的曾经"
-			}, {
-				time: "3:22",
-				lyric: "并不算爱情"
-			}, {
-				time: "3:25",
-				lyric: "为什么忽然间"
-			}, {
-				time: "3:29",
-				lyric: "我差一点相信"
-			}, {
-				time: "3:34",
-				lyric: "完美的结局"
-			}, {
-				time: "3:35",
-				lyric: "属于我和你"
-			}, {
-				time: "3:40",
-				lyric: " "
-			}, {
-				time: "3:40",
-				lyric: "怎样才可以"
-			}, {
-				time: "3:43",
-				lyric: "说服了我自己"
-			}, {
-				time: "3:48",
-				lyric: "彻底把过去"
-			}, {
-				time: "3:51",
-				lyric: "在心底抹去"
-			}, {
-				time: "3:55",
-				lyric: "如果说那幸福"
-			}, {
-				time: "3:59",
-				lyric: "不过都是错觉"
-			}, {
-				time: "4:03",
-				lyric: "为何有微笑"
-			}, {
-				time: "4:06",
-				lyric: "留在照片里"
-			}]
-		}, {
-			id: "2",
-			songs: "背影",
-			name: "林宥嘉",
-			src: "music/背影.mp3",
-			alltime: "5:54",
-			photo: "img/lyj.jpg",
-			lyrics: [{
-				time: "0:02",
-				lyric: "背影"
-			}, {
-				time: "0:13",
-				lyric: "林宥嘉"
-			}, {
-				time: "0:30",
-				lyric: ""
-			}, {
-				time: "0:31",
-				lyric: "三公分阳光 三公分空气"
-			}, {
-				time: "0:39",
-				lyric: "堵在眼前像一面 玻璃"
-			}, {
-				time: "0:46",
-				lyric: "挡住了你表情 剩下 只有脚印"
-			}, {
-				time: "1:02",
-				lyric: "一直向前走 走不完距离"
-			}, {
-				time: "1:10",
-				lyric: "一直想后退不出回忆"
-			}, {
-				time: "1:17",
-				lyric: "很高兴有心事 帮我困住自己"
-			}, {
-				time: "1:28",
-				lyric: ""
-			}, {
-				time: "1:33",
-				lyric: "你头发上淡淡青草香气"
-			}, {
-				time: "1:40",
-				lyric: "变成了风才能和我相遇"
-			}, {
-				time: "1:48",
-				lyric: "你的目光 蒸发成云"
-			}, {
-				time: "1:56",
-				lyric: "再下成雨我才能够靠近"
-			}, {
-				time: "2:02",
-				lyric: ""
-			}, {
-				time: "2:02",
-				lyric: "感谢我不可以 住进你的眼睛"
-			}, {
-				time: "2:10",
-				lyric: "所以才能 拥抱你的背影"
-			}, {
-				time: "2:18",
-				lyric: "有再多的遗憾 用来牢牢记住"
-			}, {
-				time: "2:26",
-				lyric: "不完美 的所有美丽"
-			}, {
-				time: "2:33",
-				lyric: "感谢我不可以 拥抱你的背影"
-			}, {
-				time: "2:41",
-				lyric: "所以才能 变成你的背影"
-			}, {
-				time: "2:49",
-				lyric: "躲在安静角落 不用你回头看"
-			}, {
-				time: "2:59",
-				lyric: "不用珍惜"
-			}, {
-				time: "3:05",
-				lyric: ""
-			}, {
-				time: "3:33",
-				lyric: "我怀里所有温暖的空气"
-			}, {
-				time: "3:41",
-				lyric: "变成风也不敢和你相遇"
-			}, {
-				time: "3:48",
-				lyric: "我的心事 蒸发成云"
-			}, {
-				time: "3:56",
-				lyric: "再下成雨却舍不得淋湿你"
-			}, {
-				time: "4:02",
-				lyric: ""
-			}, {
-				time: "4:03",
-				lyric: "感谢我不可以 住进你的眼睛"
-			}, {
-				time: "4:10",
-				lyric: "所以才能 拥抱你背影"
-			}, {
-				time: "4:18",
-				lyric: "有再多的遗憾 用来牢牢记住"
-			}, {
-				time: "4:27",
-				lyric: "不完美 的所有美丽"
-			}, {
-				time: "4:34",
-				lyric: "感谢我不可以 拥抱你的背影"
-			}, {
-				time: "4:41",
-				lyric: "所以才能 变成你的背影"
-			}, {
-				time: "4:49",
-				lyric: "躲在安静角落 不用你回头看"
-			}, {
-				time: "4:59",
-				lyric: "不用珍惜"
-			}, {
-				time: "5:04",
-				lyric: ""
-			}, {
-				time: "5:05",
-				lyric: "感谢我不可以 拥抱你的背影"
-			}, {
-				time: "5:12",
-				lyric: "所以才能 变成你的背影"
-			}, {
-				time: "5:20",
-				lyric: "躲在安静角落 如果你回头看"
-			}, {
-				time: "5:30",
-				lyric: "不用在意"
-			}, {
-				time: "5:35",
-				lyric: ""
-			}, {
-				time: "5:41",
-				lyric: "背影"
-			}]
-		}, {
-			id: "3",
-			songs: "红玫瑰",
-			name: "陈奕迅",
-			src: "music/红玫瑰.mp3",
-			alltime: "4:00",
-			photo: "img/cyx.jpg",
-			lyrics: [{
-				time: "0:01",
-				lyric: "红玫瑰"
-			}, {
-				time: "0:04",
-				lyric: "陈奕迅"
-			}, {
-				time: "0:06",
-				lyric: "《认了吧》"
-			}, {
-				time: "0:11",
-				lyric: "www.22lrc.com☆x大紫↗ 制作"
-			}, {
-				time: "0:16",
-				lyric: "梦里梦到醒不来的梦"
-			}, {
-				time: "0:18",
-				lyric: "红线里被软禁的红"
-			}, {
-				time: "0:23",
-				lyric: "所有刺激剩下疲乏的痛 再无动于衷"
-			}, {
-				time: "0:29",
-				lyric: "从背后抱你的时候 期待的却是她的面容"
-			}, {
-				time: "0:37",
-				lyric: "说来实在嘲讽 我不太懂 偏渴望你懂"
-			}, {
-				time: "0:44",
-				lyric: "是否幸福轻得太沉重"
-			}, {
-				time: "0:47",
-				lyric: "过度使用不痒不痛 烂熟透红空洞了的瞳孔"
-			}, {
-				time: "0:55",
-				lyric: "终于掏空 终于有始无终"
-			}, {
-				time: "0:59",
-				lyric: "得不到的永远在骚动"
-			}, {
-				time: "1:03",
-				lyric: "被偏爱的 都有恃无恐"
-			}, {
-				time: "1:06",
-				lyric: "玫瑰的红 容易受伤的梦"
-			}, {
-				time: "1:10",
-				lyric: "握在手中却流失于指缝"
-			}, {
-				time: "1:14",
-				lyric: "又落空"
-			}, {
-				time: "1:29",
-				lyric: "红是朱砂痣烙印心口"
-			}, {
-				time: "1:33",
-				lyric: "红是蚊子血般平庸"
-			}, {
-				time: "1:37",
-				lyric: "时间美化那仅有的悸动　也磨平激动"
-			}, {
-				time: "1:44",
-				lyric: "从背后抱你的时候 期待的却是她的面容"
-			}, {
-				time: "1:52",
-				lyric: "说来实在嘲讽 我不太懂 片刻望你懂"
-			}, {
-				time: "1:59",
-				lyric: "是否幸福轻得太沉重"
-			}, {
-				time: "2:03",
-				lyric: "过度使用 不痒不痛"
-			}, {
-				time: "2:06",
-				lyric: "烂熟透红空洞了的瞳孔"
-			}, {
-				time: "2:11",
-				lyric: "终于掏空 终于有始无终"
-			}, {
-				time: "2:14",
-				lyric: "得不到的永远在骚动"
-			}, {
-				time: "2:17",
-				lyric: "被偏爱的都有恃无恐"
-			}, {
-				time: "2:22",
-				lyric: "玫瑰的红 容易受伤的梦"
-			}, {
-				time: "2:25",
-				lyric: "握在手中却流失于指缝"
-			}, {
-				time: "2:29",
-				lyric: "又落空"
-			}, {
-				time: "2:33",
-				lyric: "www.22lrc.com☆x大紫↗ 制作"
-			}, {
-				time: "2:44",
-				lyric: "是否说爱都太过沉重"
-			}, {
-				time: "2:47",
-				lyric: "过度使用不痒不痛"
-			}, {
-				time: "2:52",
-				lyric: "烧得火红 蛇行缠绕心中"
-			}, {
-				time: "2:55",
-				lyric: "终于冷冻终于有始无终"
-			}, {
-				time: "2:59",
-				lyric: "得不到的永远在骚动"
-			}, {
-				time: "3:02",
-				lyric: "被偏爱的都有恃无恐"
-			}, {
-				time: "3:06",
-				lyric: "玫瑰的红 容易受伤的梦"
-			}, {
-				time: "3:11",
-				lyric: "握在手中却流失于指缝"
-			}, {
-				time: "3:14",
-				lyric: "得不到的永远在骚动"
-			}, {
-				time: "3:18",
-				lyric: "被偏爱的 都有恃无恐"
-			}, {
-				time: "3:22",
-				lyric: "玫瑰的红 伤口绽放的梦"
-			}, {
-				time: "3:25",
-				lyric: "握在手中却流失于指缝"
-			}, {
-				time: "3:29",
-				lyric: "再落空"
-			}]
-		}, {
-			id: "4",
-			songs: "初爱",
-			name: "杨宗纬",
-			src: "music/初爱.mp3",
-			alltime: "3:53",
-			photo: "img/yzw.jpg",
-			lyrics: [{
-				time: "0:01",
-				lyric: "初爱"
-			}, {
-				time: "0:02",
-				lyric: "作词：黄婷 作曲：木村充利"
-			}, {
-				time: "0:04",
-				lyric: "演唱：杨宗纬"
-			}, {
-				time: "0:07",
-				lyric: ""
-			}, {
-				time: "0:25",
-				lyric: "永远感激 你狂奔过操场 来到我眼前"
-			}, {
-				time: "0:31",
-				lyric: "阳光灿烂 烫红了你双颊 温暖你笑靥"
-			}, {
-				time: "0:36",
-				lyric: "那时节 黄澄澄的落叶 铺满整条街"
-			}, {
-				time: "0:42",
-				lyric: "下课钟声 荡过悠悠岁月"
-			}, {
-				time: "0:46",
-				lyric: "长大后 世界像一张网 网住我们的翅膀"
-			}, {
-				time: "0:54",
-				lyric: "回忆 沉甸甸在心上"
-			}, {
-				time: "0:57",
-				lyric: "偶尔 轻声独唱"
-			}, {
-				time: "0:59",
-				lyric: "是否能找回消失的力量"
-			}, {
-				time: "1:03",
-				lyric: "想起了初爱 想起最初的梦已不在"
-			}, {
-				time: "1:08",
-				lyric: ""
-			}, {
-				time: "1:09",
-				lyric: "想起青春 曾无畏无惧 无所谓失败"
-			}, {
-				time: "1:14",
-				lyric: "当时看见彩虹就笑开 一无窒碍在胸怀"
-			}, {
-				time: "1:20",
-				lyric: "带你抛下课堂 翻过围墙 只为了往一片大海"
-			}, {
-				time: "1:27",
-				lyric: "告别了初爱 告别了制服上的名牌"
-			}, {
-				time: "1:32",
-				lyric: "告别天真 学着去拨开 雨天的阴霾"
-			}, {
-				time: "1:39",
-				lyric: "沮丧失落反复地重来 不能放弃勇敢去爱"
-			}, {
-				time: "1:44",
-				lyric: "是 你让我 还相信未来"
-			}, {
-				time: "1:49",
-				lyric: ""
-			}, {
-				time: "2:04",
-				lyric: "若不是你 包容我年少时 轻狂和执拗"
-			}, {
-				time: "2:09",
-				lyric: "我不可能 在颠簸的路上 走得那么好"
-			}, {
-				time: "2:14",
-				lyric: "虽然你 终究没等到我 做你的骄傲"
-			}, {
-				time: "2:21",
-				lyric: "却永远是 我生命中的美好"
-			}, {
-				time: "2:28",
-				lyric: "总是会在碰撞中回望 脆弱累积成担当"
-			}, {
-				time: "2:33",
-				lyric: "总要 一段一段错过"
-			}, {
-				time: "2:36",
-				lyric: "愈合 那时的伤"
-			}, {
-				time: "2:39",
-				lyric: ""
-			}, {
-				time: "2:39",
-				lyric: "你却早已经不在我身旁"
-			}, {
-				time: "2:42",
-				lyric: "永远的初爱 永远最初的梦最精彩"
-			}, {
-				time: "2:47",
-				lyric: "想起青春 曾无畏无惧 无所谓失败"
-			}, {
-				time: "2:54",
-				lyric: "当时看见彩虹就笑开 一无窒碍在胸怀"
-			}, {
-				time: "3:00",
-				lyric: "带你抛下课堂 翻过围墙 只为了往一片大海"
-			}, {
-				time: "3:05",
-				lyric: "告别了初爱 告别了制服上的名牌"
-			}, {
-				time: "3:11",
-				lyric: ""
-			}, {
-				time: "3:12",
-				lyric: "告别天真 学会去拨开 雨天的阴霾"
-			}, {
-				time: "3:17",
-				lyric: "沮丧失落反复地重来 不能放弃勇敢去爱"
-			}, {
-				time: "3:23",
-				lyric: ""
-			}, {
-				time: "3:24",
-				lyric: "是 你让我 还相信未来"
-			}, {
-				time: "3:28",
-				lyric: ""
-			}, {
-				time: "3:31",
-				lyric: "我 想起你 就不会 崩坏"
-			}, {
-				time: "3:38",
-				lyric: ""
-			}]
-		}, {
-			id: "5",
-			songs: "情书",
-			name: "张学友",
-			src: "music/情书.mp3",
-			alltime: "4.06",
-			photo: "img/zxy.jpg",
-			lyrics: [{
-				time: "0:01",
-				lyric: "情书"
-			}, {
-				time: "0:01",
-				lyric: "作词 :姚若龙　作曲 :戚小恋"
-			}, {
-				time: "0:02",
-				lyric: "演唱：张学友"
-			}, {
-				time: "0:03",
-				lyric: ""
-			}, {
-				time: "0:23",
-				lyric: "你瘦了憔悴的让我好心疼"
-			}, {
-				time: "0:27",
-				lyric: "有时候爱情比时间还残忍"
-			}, {
-				time: "0:32",
-				lyric: "有人变得盲目"
-			}, {
-				time: "0:34",
-				lyric: "而奋不顾身"
-			}, {
-				time: "0:35",
-				lyric: "忘了爱 要两个同样　"
-			}, {
-				time: "0:38",
-				lyric: "用心的人"
-			}, {
-				time: "0:41",
-				lyric: "你醉了脆弱得藏不住泪痕"
-			}, {
-				time: "0:46",
-				lyric: "我知道绝望比冬天还寒冷"
-			}, {
-				time: "0:50",
-				lyric: "你恨自己是个"
-			}, {
-				time: "0:52",
-				lyric: "怕孤独的人"
-			}, {
-				time: "0:54",
-				lyric: "偏偏又爱上自由自私的灵魂"
-			}, {
-				time: "0:59",
-				lyric: ""
-			}, {
-				time: "1:03",
-				lyric: "你带着他唯一写过的情书"
-			}, {
-				time: "1:07",
-				lyric: "想证明当初爱得并不糊涂"
-			}, {
-				time: "1:11",
-				lyric: "他曾为了你的逃离颓废痛苦"
-			}, {
-				time: "1:16",
-				lyric: "也为了破镜重圆抱着你哭"
-			}, {
-				time: "1:22",
-				lyric: "哦 可惜爱不是几滴眼泪几封情书"
-			}, {
-				time: "1:27",
-				lyric: "哦 这样的话或许有点残酷"
-			}, {
-				time: "1:32",
-				lyric: ""
-			}, {
-				time: "1:33",
-				lyric: "等待着别人给幸福的人"
-			}, {
-				time: "1:36",
-				lyric: "往往过得都不怎么幸福"
-			}, {
-				time: "1:41",
-				lyric: "哦 可惜爱不是忍着眼泪留着情书"
-			}, {
-				time: "1:46",
-				lyric: "哦 伤口清醒要比昏迷痛楚"
-			}, {
-				time: "1:51",
-				lyric: "紧闭着双眼 又拖着错误"
-			}, {
-				time: "1:55",
-				lyric: "真爱 来临时你又要怎么"
-			}, {
-				time: "1:59",
-				lyric: "留得住"
-			}, {
-				time: "2:02",
-				lyric: ""
-			}, {
-				time: "2:19",
-				lyric: "你醉了脆弱得藏不住泪痕"
-			}, {
-				time: "2:24",
-				lyric: "我知道绝望比冬天还寒冷"
-			}, {
-				time: "2:28",
-				lyric: "你恨自己是个"
-			}, {
-				time: "2:31",
-				lyric: "怕孤独的人"
-			}, {
-				time: "2:32",
-				lyric: "偏偏又爱上自由自私的灵魂"
-			}, {
-				time: "2:37",
-				lyric: ""
-			}, {
-				time: "2:41",
-				lyric: "你带着他唯一写过的情书"
-			}, {
-				time: "2:45",
-				lyric: "想证明当初爱得并不糊涂"
-			}, {
-				time: "2:49",
-				lyric: "他曾为了你的逃离颓废痛苦"
-			}, {
-				time: "2:55",
-				lyric: "也为了破镜重圆抱着你哭"
-			}, {
-				time: "3:00",
-				lyric: "哦 可惜爱不是几滴眼泪几封情书"
-			}, {
-				time: "3:05",
-				lyric: "哦 这样的话或许有点残酷"
-			}, {
-				time: "3:11",
-				lyric: "等待着别人给幸福的人"
-			}, {
-				time: "3:15",
-				lyric: "往往过得都不怎么幸福"
-			}, {
-				time: "3:19",
-				lyric: "哦 可惜爱不是忍着眼泪留着情书"
-			}, {
-				time: "3:24",
-				lyric: "哦 伤口清醒要比昏迷痛楚"
-			}, {
-				time: "3:30",
-				lyric: "紧闭着双眼 又拖着错误"
-			}, {
-				time: "3:33",
-				lyric: "真爱 来临时你又要怎么"
-			}, {
-				time: "3:37",
-				lyric: "留得住"
-			}, {
-				time: "3:40",
-				lyric: ""
-			}]
-		}
-	];
-	///////////////////////
-	//歌曲列表
+//	喜欢收藏
+var col=["&#xe610;","&#xe615;"];
+var c=0;
+ $(".collect").on("click",function(){
+ 	$(".mylike .collect").css("color","#fff")
+ 	if(c==1){
+ 		c=0
+ 	}else if(c==0){
+ 		c=1
+ 		$(".mylike .collect").css("color","orange")
+ 	}
+	$(".mylike .collect").html(col[c])
+	
+})
+	//	播放模式
+	var mode = ["&#xe626;", "&#xe614;", "&#xe67c;"]
+	var s = 0;
+	$(".improt").find(".mode").on("click", function() {
+			if(s > mode.length) {
+				s = 0
+			}
+			$(mode).each(function(i, v) {
+				$(".improt").find(".mode").html(mode[s])
+
+			})
+			s = s + 1
+		})
+		///////////////////////
+		//歌曲列表
+	var index = 0;
 	var data = [{
 		song: "旅行的意义",
 		src: "music/旅行的意义.mp3",
@@ -803,33 +38,24 @@ $(function() {
 		src: "music/天空之城.mp3",
 		name: "石久让"
 	}, {
-			
-			song: "幸福的错觉",
-			name: "任重",
-			src: "music/幸福的错觉.mp3",
-			
-		}, ]
+
+		song: "幸福的错觉",
+		name: "任重",
+		src: "music/幸福的错觉.mp3",
+
+	}, ]
 	var list = $(".mune")
 		//  ........................
-		
-	var nn = $(".back")
-		nn.on("click", false)
-	nn.on("click", function() {
-		$(".mid1").removeClass("mid2").addClass("mid6")
-		$(".mid4").removeClass("mid3").addClass("mid5")
-	})
-
 	function render() {
 		list.on("click", function() {
 			$(".mid1").empty();
-			$(".progress").addClass("jingdu");
-			$(".mid1").removeClass("mid6").addClass("mid2")
-			$(".mid4").removeClass("mid5").addClass("mid3")
+			$(".progress").toggleClass("jingdu");
+			$(".mid1").toggleClass("mid6").toggleClass("mid2")
+			$(".mid4").toggleClass("mid5").toggleClass("mid3")
 			$(data).each(function(i, v) {
 				$('<li class="list"><h1>' + v.song + '</h1><p>' + v.name + '</p></li>').appendTo($(".mid1"))
 				index = $(this).index();
-				audio.play()
-					//			................
+				//			................
 			})
 		})
 	}
@@ -839,7 +65,7 @@ $(function() {
 
 	$(".cir-right").on("click", function() {
 		var indexs = index + 1;
-		if(indexs >= data.length) {
+		if(indexs > data.length) {
 			indexs = 0;
 		}
 
@@ -898,7 +124,7 @@ $(function() {
 		return m + ":" + s
 	}
 	$(audio).on("timeupdate", function() {
-			durition.text(format(audio.duration))
+			$(".durition").text(format(audio.duration))
 				//  获取总时长
 			go.text(format(audio.currentTime));
 			var left = (pro.width() - $(".pi").width() / 2) * (audio.currentTime / audio.duration)
@@ -917,6 +143,7 @@ $(function() {
 		var w = e.offsetX
 			// console.log(w)
 			// console.log(px)
+		$(document).on('mousemove', false);
 		$(document).on("mousemove", function(e) {
 			var ow = e.clientX;
 			// console.log(ow)
@@ -940,15 +167,16 @@ $(function() {
 	})
 
 	$(audio).on("progress", function() {
-
 		})
 		//	缓存结束，可以播放
 	$(audio).on("canplay", function() {
 
+			$(".header").find("span").text(data[index].song)
 		})
 		//	播放函数
 	$(audio).on("play", function() {
-
+		$(".mid").find("h1").css("color","#fff").eq(index).css("color","orange")
+		$(".mid").find("p").css("color","#E7E6EA").eq(index).css("color","orange")
 		})
 		//	事件改变函数
 	$(audio).on("timeupdate", function() {
@@ -963,7 +191,8 @@ $(function() {
 		})
 		////	结束函数
 	$(audio).on("ended", function() {
-
+		
+			audio.play()
 		})
 		//		..........................	
 
@@ -996,19 +225,23 @@ $(function() {
 		n = next
 	}
 	//	>..........左右滑动...........
-//	var first_ob;
-//	$(document).on("touchstart", function(e) {
-//		first_ob = e.originalEvent.changedTouches[0].clientX;
-//
-//	})
-//	$(document).on("touchend", function(e) {
-//		var last_ob = e.originalEvent.changedTouches[0].clientX;
-//		if(last_ob - first_ob > 50) {
-//
-//		}
-//		if(last_ob - first_ob < -50) {
-//
-//		}
-//
-//	})
+	var first_ob;
+//	$(document).on('touchstart', false);
+	$(document).on("touchstart", function(e) {
+		first_ob = e.originalEvent.changedTouches[0].clientX;
+		console.log(first_ob)
+	})
+//	$(document).on('touchend', false);
+	$(document).on("touchend", function(e) {
+		var last_ob = e.originalEvent.changedTouches[0].clientX;
+		if(last_ob - first_ob > 50) {
+			$(".mid1").romoveClass("mid6").addClass("mid2")
+			$(".mid4").romoveClass("mid5").addClass("mid3")
+		}
+		if(last_ob - first_ob < -50) {
+			$(".mid1").addClass("mid6").romoveClass("mid2")
+			$(".mid4").addClass("mid5").romoveClass("mid3")
+		}
+
+	})
 })
